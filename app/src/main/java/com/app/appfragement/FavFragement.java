b31b6e33e79c2ad66394a519ce2b7ddbf74f9f80
Nirmal <nikk3008@outlook.com>
2015-10-29 20:15:16 +0530

    New changelist

--- app/src/main/java/com/app/appfragement/FavFragement.java
+++ app/src/main/java/com/app/appfragement/FavFragement.java
@@ -2,31 +2,46 @@ package com.app.appfragement;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.*;
 import android.view.Menu;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.app.adapter.AppLibrary;
+import com.app.adapter.FavAdapter;
 import com.app.aggro.R;
-import com.app.gridcategory.ThreeWayGrid;
-import com.app.gridcategory.TwoWayGrid;
+import com.app.cards.CustomCards;
+import com.app.cards.FavCards;
+import com.app.holder.ChildItem;
+import com.app.holder.GroupItem;
+import com.app.local.database.AppTracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.recyclerview.view.CardRecyclerView;
 
 /**
  * Created by sonal on 7/2/2015.
  */
 public class FavFragement extends Fragment {
 
-    private MenuItem mSearchAction;
-    private boolean isSearchOpened = false;
-    private EditText edtSeach;
+
+    List<Card> cards;
+    CardRecyclerView mRecyclerView;
+    FavAdapter myAdapter;
 
     public static FavFragement newInstance(String imageUrl) {
 
@@ -53,17 +68,60 @@ public class FavFragement extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate and locate the main ImageView
-        final View v = inflater.inflate(R.layout.fragement_fav, container, false);
+        final View v = inflater.inflate(R.layout.fragement_app, container, false);
         init(v);
+        createcard(v);
         return v;
     }
+    private void init(View view) {
 
-
-    private void init(View view){
-        GridView gridView = (GridView)view.findViewById(R.id.gridview);
-        gridView.setAdapter(new ThreeWayGrid(getActivity()));
     }
 
 
 
+    private void createcard(View v) {
+        cards = new ArrayList<>();
+        //Create a Card
+        myAdapter = new FavAdapter(getActivity(),cards);
+
+        List<AppTracker> eventList = null;
+        eventList = AppTracker.getAllByFav(1);
+
+        for (int i = 0; i < eventList.size(); i++){
+            try {
+                AppTracker event =  eventList.get(i);
+                FavCards customCards = new FavCards(getActivity(),myAdapter);
+                customCards.setIsSystemApp(event.isSystenApp);
+                customCards.setmTitle(event.appName);
+                customCards.setmCategory(event.catName);
+                customCards.setIsFavourite(event.isFavourite);
+                if (event.isFavourite == 0)
+                    customCards.setFavResource(R.mipmap.star_green);
+                else
+                    customCards.setFavResource(R.mipmap.star_orange);
+                customCards.setmPackageName(event.packageName);
+                customCards.setPosition(i);
+                Drawable icon = getActivity().getPackageManager().getApplicationIcon(event.packageName);
+                customCards.setImageResources(icon);
+                customCards.setmRating(4);
+                cards.add(customCards);
+            }catch (PackageManager.NameNotFoundException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        myAdapter.notifyDataSetChanged();
+
+        //Staggered grid view
+        mRecyclerView = (CardRecyclerView) v.findViewById(R.id.carddemo_recyclerview);
+        mRecyclerView.setHasFixedSize(false);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //Set the empty view
+        if (mRecyclerView != null) {
+            mRecyclerView.setAdapter(myAdapter);
+        }
+
+    }
 }
